473ee1418658ab6385cf6f5213fb3091b5a9f734
Daniel Hugenroth <daniel.hugenroth@gmail.com>
2020-02-23 16:34:26 +0000

    Updated configuration for release

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="busybee-TODO", # TODO: REPLACE!
-    version="0.0.1",
-    author="Daniel Hugenroth",
-    description="Simple and interactive multi-processing for Python",
+    name="busybee",
+    version="0.1.0",
+    author="Daniel H",
+    author_email="not_provided@example.org",
+    description="Simple and interactive multi-processing for Python and notebooks",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lambdapioneer/busybee",
@@ -15,6 +16,8 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
     ],
     python_requires='>=3.6',
 )
